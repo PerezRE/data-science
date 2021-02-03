4aac2852c9bce052c8e994c1351d0e1017906ff6
I-Tocho <ismaeldjjetocho@gmail.com>
2021-02-03 10:48:40 -0600

    test

--- Postworks Lenguaje R/Postwork Sesión 8/postwork8/app.R
+++ Postworks Lenguaje R/Postwork Sesión 8/postwork8/app.R
@@ -8,14 +8,16 @@ library(ggplot2)
 #match.data <- read.csv(choose.files())  # Buscamos donde se encuentra data table
 match.data <- read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Postworks%20Lenguaje%20R/Postwork%20Sesi%C3%B3n%208/match.data.csv")
 
+
 ui <- 
     
     pageWithSidebar(
         headerPanel("Postwork 8"),
         sidebarPanel(
-            p("Crear plots con el DF 'auto'"), 
-            selectInput("x", "Seleccione el valor de X",
-                        choices = names(mtcars))
+            p("Seleccione el gráfico"), 
+            selectInput("x", "Equipo",
+                        c("Casa" = "casa",
+                          "Visitante" = "visitante"))
         ),
         mainPanel(
             
@@ -27,6 +29,10 @@ ui <-
                               height = 350, width = 450),
                          img( src = "momios_2.png", 
                               height = 350, width = 450)
+                ),
+                tabPanel("Gráficas", 
+                         plotOutput("output_plot"),
+                         plotOutput("output_plot_face")
                 ),
                 
                 tabPanel("Postwork 3",
@@ -51,6 +57,36 @@ server <- function(input, output) {
     output$data_table <- renderDataTable({match.data}, 
                                          options = list(aLengthMenu = c(20,40,60),
                                                         iDisplayLength = 20))
+    output$output_plot <- renderPlot({
+        if(input$x == "casa"){
+            home <- as.data.frame(table(match.data$home.score))
+            data <- home
+        }else{
+            away <- as.data.frame(table(match.data$away.score))
+            data <- away
+        }
+        a<- ggplot(data, aes(x = Var1, y = Freq, fill = Var1)) + 
+            geom_bar(stat = "identity") +
+            ggtitle(paste0("Equipo: ", input$x)) +
+            labs(x = "Goles", y = "Frecuencia") +
+            theme(
+                legend.position="none"
+            )
+        return(a)
+    })
+    
+    output$output_plot_face <- renderPlot({
+        b<- ggplot(match.data, aes(x = home.score, y = away.score)) + 
+            geom_bar(stat = "identity") +
+            ggtitle(paste0("Equipo ")) +
+            labs(x = "Goles Local", y = "Goles Visitante") +
+            theme(
+                legend.position="none"
+            )+
+            facet_wrap("away.team")
+        return(b)
+    })
+    
 }
 
 
