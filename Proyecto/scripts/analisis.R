b53967c85d8629f719474a59a64f9b4136eddc22
Enrique Pérez <perezre_8d@hotmail.com>
2021-02-03 20:43:40 -0600

    :bar_chart:

--- Proyecto/scripts/analisis.R
+++ Proyecto/scripts/analisis.R
@@ -4,10 +4,7 @@ library(ggplot2)
 # TODO: Escribir las interrogantes en el archivo Readme.md (solo están algunas)
 
 # Leemos los datos
-videogames <- na.omit(read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/dataset_2.csv", header=TRUE))
-
-# Eliminar variables redundantes e innecesarias
-videogames <- videogames[, -which(names(videogames) %in% c("X.1", "X", "appid", "Columna1", "Title"))]
+videogames <- na.omit(read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/dataset%202.csv", header=TRUE))
 
 # Revisamos la estructura del data frame
 class(videogames); str(videogames);
