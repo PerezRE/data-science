cd67327b8be0d65ef72d0caac2abcb5a106e30d3
I-Tocho <ismaeldjjetocho@gmail.com>
2021-02-05 14:49:42 -0600

    link update

--- Proyecto/scripts/creacion de datos.R
+++ Proyecto/scripts/creacion de datos.R
@@ -1,79 +1,87 @@
 library(tidyverse)
 
-# Abre el documento a decomponer
-data <- read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/Steam_games.csv", header = TRUE, encoding = "UTF-8")
+# Cargamos documento a descomponer
+data <- read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/steam.csv")
 
-# ========================================================================================
-# Función para extraer opciones únicas
-
-optUniques <- function(vect){
+# ================================= Funciones ==============================================#
+#Extrae opciones únicas de un vector compuesto
+optUniques<- function(vect){
   
-  filter.one <- unique(vect)
+  #Primer filtro
+  filter.one<- unique(vect)
   
-  optu <-c()
+  optu<-c()
   for (i in 1:length(filter.one)) {
+    #Extraemos y unimos las opciones en un vector
     optu <- c( optu, unlist(strsplit( filter.one[i], ";" )))  
   }
-  
+  #Comparamos y retornamos las opciones únicas
   return(unique(optu))
 }
 
-# =========================================================================================
-# Generador de columnas logicas
-
-logicalCols <- function(names, vec, nameInject){
+#Genera columnas lógicas
+logicalCols<- function(names, vec, nameInject){
   
   data<-c()
-  for (i in 1:length(vec)) {                         # Número de registros
+  for (i in 1:length(vec)) {                         
     
-    vLogical <- rep(FALSE, times = length(names))    # Vector FALSE
-    rCurrent <- unlist(strsplit( vec[i], ";" ))      # Descompone la cadena de cada fila
+    #Se crea un vector de FALSE
+    vLogical<- rep(FALSE, times = length(names))    
     
-    # Verifica coincidencia de manera ordenada
+    #Descomponemos la cadena de cada fila
+    rCurrent<- unlist(strsplit( vec[i], ";" ))      
     
+    #Verificamos coincidencia de manera ordenada
     for (j in 1:length(rCurrent)) {
-      vLogical[which(names == rCurrent[j])] = TRUE;  # Asigna coincidencias
+      #Asigna TRUE a coincidencias
+      vLogical[which(names == rCurrent[j])] = TRUE;  
     }
-    
-    data <- rbind(data, vLogical)                    # Une los datos
+    #Unimos los datos
+    data<- rbind(data, vLogical)                    
   }
   
-  for (i in 1:length(names)) {                       # Asigna nombre clave a la columna
+  #Coloca nombre clave a la columna
+  for (i in 1:length(names)) {                       
     names[i]<-paste0(nameInject, names[i])     
   }
   colnames(data)<-names
   
-  return(data)                                       # Retorna columna
+  #Retorna los datos
+  return(data)                                       
 }
+# =============================Generar columnas de otras ===================================#
+#Extraemos los vectores de referencia
+platformss<- optUniques(data$platforms)
+categoriess<- optUniques(data$categories)
+genress<- optUniques(data$genres)
 
-# ======================== Extrae vectores de búsqueda de datos ===========================
-
-platformss     <- optUniques(data$platforms)
-categoriess    <- optUniques(data$categories)
-genress        <- optUniques(data$genres)
-
-# ========================= Genera nuevas columnas para unir ==============================
-
-platformss_c  <- logicalCols(platformss, data$platforms,  "platform_")
-categoriess_c <- logicalCols(categoriess,data$categories, "categorie_")
-genress_c     <- logicalCols(genress,    data$genres,     "genre_")
+#Genera nuevas columnas para unir
+platformss_c<- logicalCols(platformss, data$platforms,  "platform_")
+categoriess_c<- logicalCols(categoriess,data$categories, "categorie_")
+genress_c<- logicalCols(genress,    data$genres,     "genre_")
 
 
-# Une columnas
+#Unimos columnas
 data <- cbind(data, platformss_c, categoriess_c, genress_c)
 
-#==========================Unir con otro dataset======================================#
-#importamos el dataset con el que vamos a unir
-juegos<-read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/metacritic_pc.csv")
+# Remueve variables de uso temporal
+remove( platformss, categoriess, genress)
+remove( platformss_c, categoriess_c, genress_c)
+
+# ============================Unir con otro dataset==========================================#
+
+#Importamos el dataset con el que vamos a unir
+juegos<-read.csv("https://raw.githubusercontent.com/PerezRE/datascience/main/Proyecto/data/metacritic.csv")
 
 #Hacemos las transformaciones necesarias para combinar el dataset
-juegos<- juegos%>%
-  mutate(nombre=str_replace_all(gsub(" ","",toupper(juegos$Title)),c("-"="",":"="","'"="")))%>%
-  arrange(nombre)%>%
+juegos<- juegos %>%
+  mutate(nombre=str_replace_all(gsub(" ","",toupper(juegos$Title)),c("-"="",":"="","'"=""))) %>%
+  arrange(nombre) %>%
   select(-Release.Date)
 
 
-datos<- data%>%mutate(nombre=str_replace_all(gsub(" ","",toupper(data$name)),c("-"="",":"="","'"="")))%>%
+datos<- data %>% 
+  mutate(nombre=str_replace_all(gsub(" ","",toupper(data$name)),c("-"="",":"="","'"=""))) %>%
   arrange(nombre)
 
 #Juntamos los datastes por nombres
@@ -81,10 +89,8 @@ data.Juegos<-merge(datos,juegos,by="nombre")
 
 #Eliminamos lo que no necesitamos
 data.Juegos<-select(data.Juegos,-nombre,-platforms,-categories,-genres)
-data.Juegos<-select(data.Juegos,-nombre)
-data.Juegos<-select(data.Juegos,-Title)
-data.Juegos<-select(data.Juegos,-Columna1)
-data.Juegos<-select(data.Juegos,-appid)
+data.Juegos<-select(data.Juegos,-Title,-Columna1,-appid)
 
-#imprimimos el csv
+#Creamos el csv
 write.csv(data.Juegos,file="DatosJuegos.csv")
+
